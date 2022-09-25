70907fcade29b0a7488b35bd25ad490c0a9505b2
InocencioC <cordeiroturorias@gmail.com>
2022-09-25 08:18:25 +0100

    array in for

---
+++ src/ConditionTerna.java
@@ -0,0 +1,10 @@
+public class ConditionTerna {
+ 
+    public static void main (String[] args) {
+        int nota = 10;
+
+        String resultado = nota >=7 ? "Aprovado" : "Reprovado";
+
+        System.out.println(resultado);
+    }
+}
